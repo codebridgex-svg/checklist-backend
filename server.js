0bfb57bcafde7d22f6ae68e070e2636d790c3a29
mozxammil <mdmozammil605@gmail.com>
2026-03-13 18:10:12 +0530

    aded

--- server.js
+++ server.js
@@ -74,7 +74,17 @@ import leaveRoutes from "./routes/leaveRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://checklist-frontend-blond.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use(express.json());
@@ -102,4 +112,4 @@ const PORT = process.env.PORT || 5000;
 // });
 
 
-app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT,() => console.log(`🚀 Server running on port ${PORT}`));
